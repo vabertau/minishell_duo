cbe4b09ca0426e327772489af2bab88c02fc1e45
hezaz <hedizaz@outlook.fr>
2024-04-25 15:35:14 +0200

    exec avec command not found

--- srcs/executor.c
+++ srcs/executor.c
@@ -6,7 +6,7 @@
 /*   By: hzaz <hzaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:43:54 by hzaz              #+#    #+#             */
-/*   Updated: 2024/04/25 15:33:05 by hzaz             ###   ########.fr       */
+/*   Updated: 2024/04/25 15:34:51 by hzaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,7 +132,7 @@ int exec_cmd(t_data *shell, t_exec *cmd)
 						execve(ret, f, shell->env);
 
 			}
-			printf("command not found: %s\n", ++f[0]);
+			ft_printf("command not found: %s\n", ++f[0]);
 		}
 	}
 	return 0;
