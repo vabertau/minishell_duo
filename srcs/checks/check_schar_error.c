2538c5646752e501d9f01e24c0c9316e5edc3efd
Valentin Bertaud <vabertau@bess-f3r1s14.clusters.42paris.fr>
2024-05-09 16:55:05 +0200

    norm

--- srcs/checks/check_schar_error.c
+++ srcs/checks/check_schar_error.c
@@ -6,7 +6,7 @@
 /*   By: vabertau <vabertau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 13:06:04 by vabertau          #+#    #+#             */
-/*   Updated: 2024/05/09 16:28:27 by vabertau         ###   ########.fr       */
+/*   Updated: 2024/05/09 16:54:47 by vabertau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,64 +63,30 @@ static int	check_for_append_pipe(t_data *data)
 	return (0);
 }
 
-/*
-Checks for >    > return 1 if that error is found
-*/
-static int	check_for_append_space_append(t_data *data)
-{
-	int		i;
-	char	*cmdline;
-	bool	space_found;
-
-	i = 0;
-	space_found = 0;
-	cmdline = data->cmdline;
-	while (cmdline[i])
-	{
-		if (cmdline[i] == '\'')
-			i += skip_sq(&(cmdline[i]));
-		if (cmdline[i] == '\"')
-			i += skip_dq(&(cmdline[i]));
-		if (cmdline[i] == '<')
-		{
-			i++;
-			while (cmdline[i] == ' ')
-			{
-				i++;
-				space_found = 1;
-			}
-			if (cmdline[i] == '<' || cmdline[i] == '>')
-			{
-				if (space_found == 1)
-					return (1);
-			}
-			space_found = 0;
-		}
-		if (cmdline[i] == '>')
-		{
-			i++;
-			while (cmdline[i] == ' ')
-			{
-				i++;
-				space_found = 1;
-			}
-			if (cmdline[i] == '<' || cmdline[i] == '>')
-			{
-				if (space_found == 1)
-					return (1);
-			}
-			space_found = 0;
-		}
-		i++;
-	}
-	return (0);
-}
-
 int	check_forbidden(char c)
 {
 	if (c == '\\' || c == ';' || c == '\0' || c == '&' || c == '*' || c == '('
 		|| c == ')')
 		return (1);
+	return (0);
+}
+
+static int	check_schar_inception(t_data *data, char *cmdline, int i)
+{
+	if (cmdline[i] == '|')
+		if (check_if_next_is_pipe(&(cmdline[i])) == 1)
+			return ((void)parsing_error(data), 1);
+	if (cmdline[i + 1] && ((cmdline[i] == '>' && cmdline[i + 1] == '<')
+			|| (cmdline[i] == '<' && cmdline[i + 1] == '>')))
+		return ((void)parsing_error(data), 1);
+	if ((cmdline[i + 1] && cmdline[i + 2]) && ((cmdline[i] == '<'
+				&& cmdline[i + 1] == '<' && (cmdline[i + 2] == '<'
+					|| cmdline[i + 2] == '>')) || (cmdline[i] == '>'
+				&& cmdline[i + 1] == '>' && (cmdline[i + 2] == '>'
+					|| cmdline[i + 2] == '<'))))
+		return ((void)parsing_error(data), 1);
+	if (check_forbidden(cmdline[i]) == 1)
+		return ((void)parsing_error(data), 1);
 	return (0);
 }
 
@@ -140,20 +106,8 @@ void	check_schar_error(t_data *data)
 			i += skip_sq(&(cmdline[i]));
 		if (cmdline[i] == '\"')
 			i += skip_dq(&(cmdline[i]));
-		if (cmdline[i] == '|')
-			if (check_if_next_is_pipe(&(cmdline[i])) == 1)
-				return ((void)parsing_error(data));
-		if (cmdline[i + 1] && ((cmdline[i] == '>' && cmdline[i + 1] == '<')
-				|| (cmdline[i] == '<' && cmdline[i + 1] == '>')))
-			return ((void)parsing_error(data));
-		if ((cmdline[i + 1] && cmdline[i + 2]) && ((cmdline[i] == '<'
-					&& cmdline[i + 1] == '<' && (cmdline[i + 2] == '<'
-						|| cmdline[i + 2] == '>')) || (cmdline[i] == '>'
-					&& cmdline[i + 1] == '>' && (cmdline[i + 2] == '>'
-						|| cmdline[i + 2] == '<'))))
-			return ((void)parsing_error(data));
-		if (check_forbidden(cmdline[i]) == 1)
-			return ((void)parsing_error(data));
+		if (check_schar_inception(data, cmdline, i))
+			return ;
 		i++;
 	}
 	if (check_for_append_space_append(data) == 1)
